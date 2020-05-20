6d98cf549b562bcedb80253cc8ac6631b69dbca9
swamy342 <manikantaswamyios@gmail.com>
2020-05-20 21:30:30 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -1,6 +1,6 @@
 class Simple{   
     public static void main(String args[]) throws InterruptedException{   
      Thread.sleep(10000); 
-     System.out.println("Hello Java");   
+     System.out.println("Hello Java it is executed in may20th 2020");   
     }   
 } 
